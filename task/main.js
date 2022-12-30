4db9e8719e0a56cb5909c50f665965ea24c48260
Ethan Thomure <ethanthomure@icloud.com>
2022-12-30 11:27:50 -0600

    Stage 3

--- task/main.js
+++ task/main.js
@@ -1,3 +1,4 @@
+const input = require('sync-input');
 const giftList = {
     1: {name: "Teddy Bear", price: 10},
     2: {name: "Big Red Ball", price: 5},
@@ -10,10 +11,37 @@ const giftList = {
     9: {name: "Basketball", price: 20},
     10: {name: "Scary Mask", price: 75}};
 
-console.log(`WELCOME TO THE CARNIVAL GIFT SHOP!
-Hello friend! Thank you for visiting the carnival!
-Here's the list of gifts:
-`)
-for (let giftID in giftList) {
-    console.log(`${giftID}- ${giftList[giftID].name}, Cost: ${giftList[giftID].price} tickets`);
+function showGiftList()  {
+    console.log("Here's the list of gifts:\n");
+    for (let giftID in giftList) {
+        console.log(`${giftID}- ${giftList[giftID].name}, Cost: ${giftList[giftID].price} tickets`);
+    }
 }
+
+
+let totalTickets = 100;
+console.log(`WELCOME TO THE CARNIVAL GIFT SHOP!
+Hello friend! Thank you for visiting the carnival!`);
+showGiftList();
+
+let action = input("\nWhat do you want to do?\n1-Buy a gift 2-Add tickets 3-Check tickets 4-Show gifts\n");
+switch (action) {
+    case "1":
+        let giftChoiceID = input("Enter the number of the gift you want to get: ");
+        console.log(`Here you go, one ${giftList[giftChoiceID].name}!`);
+        totalTickets -= giftList[giftChoiceID].price;
+        console.log(`Total tickets: ${totalTickets}`);
+        break;
+    case "2":
+        let ticketAddAmount = Number(input("Enter the ticket amount: "));
+        totalTickets += ticketAddAmount;
+        console.log(`Total tickets: ${totalTickets}`);
+        break;
+    case "3":
+        console.log(`Total tickets: ${totalTickets}`);
+        break;
+    case "4":
+        showGiftList();
+        break;
+}
+console.log("Have a nice day!");
