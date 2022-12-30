f39ae50d29d808bb5d08afc32fbbee37fff3a70d
Ethan Thomure <ethanthomure@icloud.com>
2022-12-30 12:38:25 -0600

    Stage 4

--- task/main.js
+++ task/main.js
@@ -1,5 +1,5 @@
 const input = require('sync-input');
-const giftList = {
+let giftList = {
     1: {name: "Teddy Bear", price: 10},
     2: {name: "Big Red Ball", price: 5},
     3: {name: "Huge Bear", price: 50},
@@ -19,29 +19,33 @@ function showGiftList()  {
 }
 
 
-let totalTickets = 100;
 console.log(`WELCOME TO THE CARNIVAL GIFT SHOP!
 Hello friend! Thank you for visiting the carnival!`);
 showGiftList();
 
-let action = input("\nWhat do you want to do?\n1-Buy a gift 2-Add tickets 3-Check tickets 4-Show gifts\n");
-switch (action) {
-    case "1":
-        let giftChoiceID = input("Enter the number of the gift you want to get: ");
-        console.log(`Here you go, one ${giftList[giftChoiceID].name}!`);
-        totalTickets -= giftList[giftChoiceID].price;
-        console.log(`Total tickets: ${totalTickets}`);
-        break;
-    case "2":
-        let ticketAddAmount = Number(input("Enter the ticket amount: "));
-        totalTickets += ticketAddAmount;
-        console.log(`Total tickets: ${totalTickets}`);
-        break;
-    case "3":
-        console.log(`Total tickets: ${totalTickets}`);
-        break;
-    case "4":
-        showGiftList();
-        break;
-}
+let totalTickets = 0;
+let action = ""
+do {
+    action = input("\nWhat do you want to do?\n1-Buy a gift 2-Add tickets 3-Check tickets 4-Show gifts 5-Exit the shop\n");
+    switch (action) {
+        case "1":
+            let giftChoiceID = input("Enter the number of the gift you want to get: ");
+            console.log(`Here you go, one ${giftList[giftChoiceID].name}!`);
+            totalTickets -= giftList[giftChoiceID].price;
+            delete giftList[giftChoiceID];
+            console.log(`Total tickets: ${totalTickets}`);
+            break;
+        case "2":
+            let ticketAddAmount = Number(input("Enter the ticket amount: "));
+            totalTickets += ticketAddAmount;
+            console.log(`Total tickets: ${totalTickets}`);
+            break;
+        case "3":
+            console.log(`Total tickets: ${totalTickets}`);
+            break;
+        case "4":
+            showGiftList();
+            break;
+    }
+} while (!(action === "5"));
 console.log("Have a nice day!");
