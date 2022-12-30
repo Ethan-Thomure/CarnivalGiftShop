716b7c75cc7061b97ec25dcb7bf46af02b6955c9
Ethan Thomure <ethanthomure@icloud.com>
2022-12-30 10:49:21 -0600

    Stage 2

--- task/main.js
+++ task/main.js
@@ -1,10 +1,19 @@
-const giftList = ["Teddy Bear", "Big Red Ball", "Huge Bear", "Candy", "Stuffed Tiger", "Stuffed Dragon",
-    "Skateboard", "Toy Car", "Basketball", "Scary Mask"];
+const giftList = {
+    1: {name: "Teddy Bear", price: 10},
+    2: {name: "Big Red Ball", price: 5},
+    3: {name: "Huge Bear", price: 50},
+    4: {name: "Candy", price: 8},
+    5: {name: "Stuffed Tiger", price: 15},
+    6: {name: "Stuffed Dragon", price: 30},
+    7: {name: "Skateboard", price: 100},
+    8: {name: "Toy Car", price: 25},
+    9: {name: "Basketball", price: 20},
+    10: {name: "Scary Mask", price: 75}};
 
 console.log(`WELCOME TO THE CARNIVAL GIFT SHOP!
 Hello friend! Thank you for visiting the carnival!
 Here's the list of gifts:
 `)
-giftList.forEach(function(gift) {
-    console.log(gift);
-});
+for (let giftID in giftList) {
+    console.log(`${giftID}- ${giftList[giftID].name}, Cost: ${giftList[giftID].price} tickets`);
+}
