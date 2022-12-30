b622e9d3c3fce4c0671dd500114ac1f08527f000
Ethan Thomure <ethanthomure@icloud.com>
2022-12-30 13:59:25 -0600

    Stage 5

--- task/main.js
+++ task/main.js
@@ -13,8 +13,12 @@ let giftList = {
 
 function showGiftList()  {
     console.log("Here's the list of gifts:\n");
-    for (let giftID in giftList) {
-        console.log(`${giftID}- ${giftList[giftID].name}, Cost: ${giftList[giftID].price} tickets`);
+    if (Object.keys(giftList).length === 0) {
+        console.log("\nWow! There are no gifts to buy.");
+    } else {
+        for (let giftID in giftList) {
+            console.log(`${giftID}- ${giftList[giftID].name}, Cost: ${giftList[giftID].price} tickets`);
+        }
     }
 }
 
@@ -29,23 +33,50 @@ do {
     action = input("\nWhat do you want to do?\n1-Buy a gift 2-Add tickets 3-Check tickets 4-Show gifts 5-Exit the shop\n");
     switch (action) {
         case "1":
+            if (Object.keys(giftList).length === 0) {
+                console.log("Wow! There are no gifts to buy.");
+                break;
+            }
             let giftChoiceID = input("Enter the number of the gift you want to get: ");
-            console.log(`Here you go, one ${giftList[giftChoiceID].name}!`);
-            totalTickets -= giftList[giftChoiceID].price;
-            delete giftList[giftChoiceID];
-            console.log(`Total tickets: ${totalTickets}`);
-            break;
+            if (isNaN(Number(giftChoiceID))) {
+                console.log("Please enter a valid number!");
+                break;
+            } else if (giftChoiceID in giftList) {
+                if (totalTickets < giftList[giftChoiceID].price) {
+                    console.log("You don't have enough tickets to buy this gift.");
+                    break;
+                }
+                console.log(`Here you go, one ${giftList[giftChoiceID].name}!`);
+                totalTickets -= giftList[giftChoiceID].price;
+                delete giftList[giftChoiceID];
+                console.log(`Total tickets: ${totalTickets}`);
+                break;
+            } else {
+                console.log("There is no gift with that number!");
+                break;
+            }
+
         case "2":
             let ticketAddAmount = Number(input("Enter the ticket amount: "));
-            totalTickets += ticketAddAmount;
-            console.log(`Total tickets: ${totalTickets}`);
-            break;
+            if (ticketAddAmount >= 0 && ticketAddAmount <= 1000) {
+                totalTickets += ticketAddAmount;
+                console.log(`Total tickets: ${totalTickets}`);
+                break;
+            } else {
+                console.log("Please enter a valid number between 0 and 1000.");
+                break;
+            }
+
         case "3":
             console.log(`Total tickets: ${totalTickets}`);
             break;
         case "4":
             showGiftList();
             break;
+        case "5":
+            break;
+        default:
+            console.log("Please enter a valid number!");
     }
 } while (!(action === "5"));
 console.log("Have a nice day!");
